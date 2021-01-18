d517761d40e59c3d09df45bd3c8ca8d74164b1da
zhaotianxin <ztxemail@163.com>
2021-01-18 20:46:31 +0800

    [IMP] 初始化项目结构

---
+++ src/test/java/com/code/report/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.report.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
